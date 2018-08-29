d11cbf018f684e477857168cb188c264ec4f7967
Tess Monical <tmonical@tmonical-m1.local>
2018-08-29 11:34:31 -0500

    add getOneTag function

---
+++ backend/podfics/tags.js
@@ -0,0 +1,25 @@
+const AWS = require("aws-sdk");
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.getOne = (event, context, callback) => {
+    const params = {
+      TableName: process.env.TAGS_TABLE,
+      KeyConditionExpression: "id = :id",
+      ExpressionAttributeValues: {
+        ":id": event.pathParameters.id
+      }
+    };
+  
+    dynamoDb
+      .query(params)
+      .promise()
+      .then(items => items.Items)
+      .then(result => {
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify(result)
+        };
+        callback(null, response);
+      });
+  };
+  
