724d9c3462880fa865186a44151d70e0a1350ad6
Tess Monical <tmonical@tmonical-m1.local>
2018-08-29 13:30:26 -0500

    add tags to backend podfic requests

--- backend/podfics/podfics.js
+++ backend/podfics/podfics.js
@@ -1,5 +1,6 @@
 const AWS = require("aws-sdk");
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const getOneTag = require("./tags").getOne;
 
 module.exports.getAll = (event, context, callback) => {
   const params = {
@@ -11,9 +12,39 @@ module.exports.getAll = (event, context, callback) => {
     .promise()
     .then(items => items.Items)
     .then(items => {
+      // get a list of all tags we need to fetch
+      const totalTags = items.reduce((acc, item) => {
+        item.tagIds.forEach(tag => {
+          acc[tag] = true;
+        });
+        return acc;
+      }, {});
+
+      // fetch 'em
+      const tagList = Object.keys(totalTags);
+      const tagPromises = tagList.map(tag => {
+        const tagParams = {
+          TableName: process.env.TAGS_TABLE,
+          KeyConditionExpression: "id = :id",
+          ExpressionAttributeValues: {
+            ":id": tag
+          }
+        };
+        return dynamoDb
+          .query(tagParams)
+          .promise()
+          .then(tag => tag.Items);
+      });
+
+      return Promise.all([items, Promise.all(tagPromises)]);
+    })
+    .then(([podfics, tags]) => {
       const response = {
         statusCode: 200,
-        body: JSON.stringify(items)
+        body: JSON.stringify({
+          podfics,
+          tags
+        })
       };
       callback(null, response);
     })
