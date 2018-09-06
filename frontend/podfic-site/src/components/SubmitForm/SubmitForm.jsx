2cab4b85f683967ff863985c9d881ef8f7b0805d
Tess Monical <tmonical@tmonical-m1.local>
2018-09-06 15:12:17 -0500

    more fields and some styling

--- frontend/podfic-site/src/components/SubmitForm/SubmitForm.jsx
+++ frontend/podfic-site/src/components/SubmitForm/SubmitForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from "react";
+import "./submitform.css"
 
 class SubmitForm extends Component {
   constructor(props) {
@@ -8,14 +9,17 @@ class SubmitForm extends Component {
       textUrl: "",
       imageFile: null,
       files: [{ description: "", file: null }],
-      author: "",
+      writer: "",
       authorUrl: "",
       reader: "",
       readerUrl: "",
+      contactEmail: "",
       permissionCheckbox: false
     };
     this.removeFile = this.removeFile.bind(this);
+    this.addFile = this.addFile.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.validateForm = this.validateForm.bind(this);
   }
 
   removeFile(indexToRemove) {
@@ -24,8 +28,42 @@ class SubmitForm extends Component {
     });
   }
 
+  addFile() {
+    this.setState({
+      files: [...this.state.files, { description: "", file: null }]
+    });
+  }
+
   handleInputChange(type, text) {
     this.setState({ [type]: text });
+  }
+
+  handleFileDescriptionChange(index, newText) {
+    const newFiles = this.state.files.slice();
+    newFiles[index].description = newText;
+    this.setState({
+      files: newFiles
+    });
+  }
+
+  validateForm() {
+    const {
+      title,
+      files,
+      writer,
+      reader,
+      textUrl,
+      permissionCheckbox
+    } = this.state;
+
+    if (title === "" || writer === "" || reader === "" || textUrl === "")
+      return false;
+
+    if (files.length < 1) return false;
+
+    if (!permissionCheckbox) return false;
+
+    return true;
   }
 
   render() {
@@ -33,33 +71,127 @@ class SubmitForm extends Component {
       title,
       imageFile,
       files,
-      author,
-      authorUrl,
+      writer,
+      writerUrl,
       reader,
       readerUrl,
       textUrl,
+      contactEmail,
       permissionCheckbox
     } = this.state;
     return (
-      <div>
+      <div className="submit-form">
         <form>
-          <input name="title" value={title} />
-          <input name="author" value={author} />
+          <div>
+            <label htmlFor="title">Title</label>
+            <input
+              id="title"
+              value={title}
+              onChange={e => this.handleInputChange("title", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="image-upload">Image File</label>
+            <input id="image-upload" type="file" />
+          </div>
+
+          <div>
+            <label htmlFor="writer">Writer Name</label>
+            <input
+              id="writer"
+              value={writer}
+              onChange={e => this.handleInputChange("writer", e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="writer-url">Writer Url</label>
+            <input
+              id="writer-url"
+              value={writerUrl}
+              onChange={e =>
+                this.handleInputChange("writerUrl", e.target.value)
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="reader">Reader</label>
+            <input
+              id="reader"
+              value={reader}
+              onChange={e => this.handleInputChange("reader", e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="reader-url">Reader Url</label>
+            <input
+              id="reader-url"
+              value={readerUrl}
+              onChange={e =>
+                this.handleInputChange("readerUrl", e.target.value)
+              }
+            />
+          </div>
           {files.map((file, i) => (
-            <Fragment>
-              <label htmlFor={`file${i + 1}`}>{`Audio File ${i + 1}`}</label>
-              <input name={`file${i + 1}`} type="file" value={file.file} />
-              <label
-                htmlFor={`filename${i + 1}`}
-              >{`Short Description of Audio file ${i + 1}`}</label>
-              <input
-                name={`filename${i + 1}`}
-                type="text"
-                value={file.description}
-              />
-            </Fragment>
+            <div>
+              <div>
+                <label htmlFor={`file${i + 1}`}>{`Audio File ${i + 1}`}</label>
+                <input id={`file${i + 1}`} type="file" />
+              </div>
+              <div>
+                <label
+                  htmlFor={`filename${i + 1}`}
+                >{`Short Description of Audio file ${i + 1}`}</label>
+                <input
+                  id={`filename${i + 1}`}
+                  type="text"
+                  value={file.description}
+                />
+              </div>
+              <button
+                className="remove-button"
+                type="button"
+                onClick={() => this.removeFile(i)}
+              >
+                X
+              </button>
+            </div>
           ))}
-          <button type="submit">Submit</button>
+          <button type="button" onClick={this.addFile}>
+            Add another file
+          </button>
+
+          <div>
+            <input
+              id="agreeCheckbox"
+              type="checkbox"
+              checked={permissionCheckbox}
+            />
+            <label htmlFor="agreeCheckbox">
+              I agree to the terms and conditions:
+              <ol>
+                <li>
+                  Don't upload copyrighted music or other audio except as
+                  allowed by USA fair use rules- we don't want to get in
+                  trouble.
+                </li>
+                <li>
+                  Get permission to podfic other's work. We don't check you on
+                  this- but if we get complaints, your work may be taken down
+                </li>
+                <li>
+                  Please maintain backups of your own works. While we will make
+                  every effort to keep files from getting lost, this site is
+                  very much in beta and it is possible that data may be lost
+                </li>
+                <li>Don't be a jerk. Don't harrass other users in any way.</li>
+              </ol>
+            </label>
+          </div>
+
+          <button type="submit" disabled={!this.validateForm()}>
+            Submit
+          </button>
         </form>
       </div>
     );
