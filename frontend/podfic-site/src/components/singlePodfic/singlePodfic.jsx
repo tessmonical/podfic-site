5558f18a198bbc99ce39e990f3c8d073b295cd6b
Tess Monical <tmonical@tmonical-m1.local>
2018-09-06 14:01:28 -0500

    better styling

--- frontend/podfic-site/src/components/singlePodfic/singlePodfic.jsx
+++ frontend/podfic-site/src/components/singlePodfic/singlePodfic.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
 import { AudioPlayer } from "../AudioPlayer";
@@ -20,31 +20,45 @@ class SinglePodfic extends Component {
       files
     } = podfic;
     return (
-      <div className="single-podfic">
-        <Link to={`/podfic/${id}`}>
-          <span className="podfic-title">{title}</span>
-        </Link>
-        <div className="reader-writer">
-          Written by {writer || "unknown"}, read by {reader || "unknown"}
-        </div>
-        <div className="posted-updated">
-          Posted on {moment(createdDate).format("YYYY-MM-DD")}, updated on{" "}
-          {moment(updatedDate).format("YYYY-MM-DD")}
-        </div>
-        <div className="tags">
-          {tags &&
-            tags.map(tag => {
-              return <Link key={tag.id} to={`tags/${tag.id}`} />;
-            })}
+      <Fragment>
+        <div className="single-podfic">
+          <div className={expanded ? "podfic-image expanded" : "podfic-image"}>
+            {imageUrl && <img src={imageUrl} />}
+          </div>
+          <div className="podfic-info-box">
+            <div className="podfic-title">
+              <Link to={`/podfic/${id}`}>
+                <span className="podfic-title-span">{title}</span>
+              </Link>
+            </div>
+            <div className="reader-writer">
+              Written by {writer || "unknown"}, read by {reader || "unknown"}
+            </div>
+            <div className="posted-updated">
+              Posted on {moment(createdDate).format("YYYY-MM-DD")}, updated on{" "}
+              {moment(updatedDate).format("YYYY-MM-DD")}
+            </div>
+            <div className="tags">
+              <span>Tags: </span>
+              {tags &&
+                tags.map(tag => {
+                  return (
+                    <Link key={tag.id} to={`tag/${tag.id}`}>
+                      {tag.name}
+                    </Link>
+                  );
+                })}
+            </div>
+          </div>
         </div>
         {expanded && (
           <div className="audio-files-list">
             {files.map(file => (
-              <AudioPlayer key={file.id }file={file} />
+              <AudioPlayer key={file.id} file={file} />
             ))}
           </div>
         )}
-      </div>
+      </Fragment>
     );
   }
 }
